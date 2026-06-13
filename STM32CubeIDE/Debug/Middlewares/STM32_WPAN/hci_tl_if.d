4002949bf02b0a7d09a3295066ba53425dd14345
Dawid <sztynks>
2026-06-13 22:15:05 +0200

    add BMP180 temperature sensor

---
+++ STM32CubeIDE/Debug/Middlewares/STM32_WPAN/hci_tl_if.d
@@ -0,0 +1,12 @@
+Middlewares/STM32_WPAN/hci_tl_if.o: \
+ C:/Users/dawi2/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/hci_tl_if.c \
+ C:/Users/dawi2/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/hci_tl.h \
+ ../../Middlewares/ST/STM32_WPAN/stm32_wpan_common.h \
+ ../../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../../Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/dawi2/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/tl.h
+C:/Users/dawi2/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/hci_tl.h:
+../../Middlewares/ST/STM32_WPAN/stm32_wpan_common.h:
+../../Drivers/CMSIS/Include/cmsis_compiler.h:
+../../Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/dawi2/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/tl.h:
---
+++ STM32CubeIDE/Debug/Middlewares/STM32_WPAN/osal.d
@@ -0,0 +1,4 @@
+Middlewares/STM32_WPAN/osal.o: \
+ C:/Users/dawi2/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Middlewares/ST/STM32_WPAN/ble/core/template/osal.c \
+ C:/Users/dawi2/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Middlewares/ST/STM32_WPAN/ble/core/template/osal.h
+C:/Users/dawi2/STM32CubeIDE/workspace_1.19.0/BLE_p2pServer/Middlewares/ST/STM32_WPAN/ble/core/template/osal.h:
